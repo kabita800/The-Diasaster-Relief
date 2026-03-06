fe98384f007d2b39a150c0e985651049c4911b88
8972-amick <khadkaamick2@gmail.com>
2026-03-06 19:13:17 +0545

    backend: signup backed code written

---
+++ backend/controllers/authControllers.js
@@ -0,0 +1,36 @@
+import bycrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import prisma from "../prisma/prismaClient.js";
+
+export const signUp = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+    if (user) {
+      return res
+        .status(400)
+        .json({ message: "User already exists", success: false });
+    }
+    const hashedPassword = await bycrypt.hash(password, 10);
+    const newUser = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+    return res
+      .status(201)
+      .json({
+        message: "User created successfully",
+        success: true,
+        user: newUser,
+      });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Internal server error", success: false });
+  }
+};
