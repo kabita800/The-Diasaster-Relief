6565f50befac5eb5b07dbfa060e427f69d058913
kabita <kabeetagiree45@gmail.com>
2026-03-10 06:52:30 +0545

    backendd

--- frontend/src/pages/authentication/Login.jsx
+++ frontend/src/pages/authentication/Login.jsx
@@ -2,6 +2,7 @@
 
 import { Link } from "react-router-dom";
 
+//login page
 export default function Login() {
   return (
     <div className="bg-gray-100 flex min-h-screen flex-col justify-center px-6 py-12 lg:px-8">
