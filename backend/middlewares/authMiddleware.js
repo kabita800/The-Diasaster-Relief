21e7a0845892e445c9e24d20e2d8c2c2d78c5460
kabita <kabeetagiree45@gmail.com>
2026-03-10 21:13:08 +0545

    small changes

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 
 export const authMiddleware = (req, res, next) => {
     try {
-        const token = req.headers.authorization?.split(" ")[1];
+        const token = req.headers.authorization?.split(" ")[1]; 
         if (!token) {
             return 
             res.status(401)
