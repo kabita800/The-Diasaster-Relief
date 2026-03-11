f18ebf9bf2c8e6fe6cf493bc3285ac60435e1df2
kabita <kabeetagiree45@gmail.com>
2026-03-11 21:40:17 +0545

    used cors functionality to integrate with frontend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 import dotenv from "dotenv";
 dotenv.config();
-
+import cors from "cors";
 import express from "express";
 import authRoutes from "./Routes/authRoutes.js";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 // Routes
